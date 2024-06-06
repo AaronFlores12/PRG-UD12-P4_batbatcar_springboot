489aee5c4f8ede30f1435b0a9108d04f86ff66dd
Aarón Flores Sanchez <aaronfloressanchez@Ordenador-portatil-de-Aaron.local>
2024-06-06 18:10:23 +0200

    Validator realizado y fase 1 terminada

--- src/main/java/es/batbatcar/v2p4/controllers/ViajesController.java
+++ src/main/java/es/batbatcar/v2p4/controllers/ViajesController.java
@@ -6,6 +6,7 @@ import es.batbatcar.v2p4.exceptions.ViajeNotFoundException;
 import es.batbatcar.v2p4.modelo.dto.viaje.Viaje;
 import es.batbatcar.v2p4.modelo.repositories.ViajesRepository;
 
+import es.batbatcar.v2p4.utils.Validator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -54,22 +55,48 @@ public class ViajesController {
 
     @PostMapping(value = "/viaje/add")
     public String addViaje(@RequestParam Map<String, String> params, Model model, RedirectAttributes redirectAttributes) {
+        HashMap<String, String> errors = new HashMap<>();
         try{
             int codViaje = viajesRepository.getNextCodViaje();
             String propietario = params.get("propietario");
+            if (!Validator.isValidPropietario(propietario)){
+                errors.put("propietario", "El propietario debe contener nombre y apellidos, ambos con" +
+                        "la primera letra en mayuscula");
+            }
             String ruta = params.get("ruta");
+            if (!Validator.isValidRoute(ruta)){
+                errors.put("ruta","La ruta debe contener el formato 'Origen - Destino'");
+            }
             LocalDate diaSalida = LocalDate.parse(params.get("diaSalida"));
             LocalTime horaSalida = LocalTime.parse(params.get("horaSalida"));
             LocalDateTime fechaSalida = LocalDateTime.of(diaSalida, horaSalida);
+            if (!Validator.isValidDate(String.valueOf(diaSalida))){
+                errors.put("diaSalida","El dia de salida debe tener el formato yyyy-MM-dd");
+            }
+            if (!Validator.isValidTime(String.valueOf(horaSalida))){
+                errors.put("horaSalida","El hora debe tener el formato HH:mm");
+            }
             long duracion = Long.parseLong(params.get("duracion"));
+            if (!Validator.isPositiveInt((int) duracion)){
+                errors.put("duracion","El duracion debe ser positivo");
+            }
             float precio = Float.parseFloat(params.get("precio"));
+            if (!Validator.isPositiveFloat(precio)){
+                errors.put("precio","El precio debe ser positivo");
+            }
             int plazasOfertadas = Integer.parseInt(params.get("plazasOfertadas"));
+            if (!Validator.isPositiveInt(plazasOfertadas)){
+                errors.put("plazasOfertadas","Las plazas ofertadas deben ser positivos");
+            }
+            if (errors.isEmpty()){
+                redirectAttributes.addFlashAttribute("errors", errors);
+                return "redirect:/viaje/add";
+            }
             Viaje viaje = new Viaje(codViaje,propietario,ruta,fechaSalida,duracion,precio,plazasOfertadas);
             viajesRepository.save(viaje);
             redirectAttributes.addFlashAttribute("infoMessage", "Viaje agregado con exito");
             return "redirect:/viajes";
         } catch (ViajeAlreadyExistsException | ViajeNotFoundException e) {
-            HashMap<String, String> errors = new HashMap<>();
             errors.put("error: ","El viaje ya existe" + e.getMessage());
             redirectAttributes.addFlashAttribute("errors", errors);
             return "redirect:/viajes";
--- src/main/java/es/batbatcar/v2p4/utils/Validator.java
+++ src/main/java/es/batbatcar/v2p4/utils/Validator.java
@@ -1,13 +1,38 @@
 package es.batbatcar.v2p4.utils;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
 public class Validator {
 
-	// Incluye aquí el resto de métodos de validación que necesites
-	
+    public static boolean isValidName(String param) {
+        return isNotEmptyOrNull(param) && param.length() >= 5 && Character.isUpperCase(param.charAt(0));
+    }
+
+    private static boolean isNotEmptyOrNull(String param) {
+        return param != null && !param.isEmpty();
+    }
+
+    // Incluye aquí el resto de métodos de validación que necesites
+
+    public static boolean isValidRoute(String route) {
+        return isNotEmptyOrNull(route) && route.matches("^[a-zA-Z]+\\s?-\\s?[a-zA-Z]+$");
+    }
+
+    public static boolean isValidPropietario(String propietario) {
+        return isNotEmptyOrNull(propietario) && propietario.matches("^[A-Z][a-zA-Z]\\s[A-Z][a-zA-Z]$");
+    }
+
+    public static boolean isPositiveInt(int number) {
+        return number > 0;
+    }
+
+    public static boolean isPositiveFloat(float number) {
+        return number > 0;
+    }
+
     public static boolean isValidDateTime(String dateTime) {
         try {
             LocalDate.parse(dateTime, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
@@ -27,12 +52,15 @@ public class Validator {
 
     public static boolean isValidTime(String time) {
         try {
-            LocalDate.parse(time, DateTimeFormatter.ofPattern("HH:mm"));
+            LocalTime.parse(time, DateTimeFormatter.ofPattern("HH:mm"));
         } catch (DateTimeParseException e) {
             return false;
         }
         return true;
     }
 
-}
+    public static boolean isValidPlazasOfertadas(int plazas) {
+        return plazas >= 1 && plazas <= 6;
+    }
 
+}
